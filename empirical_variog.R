6d500312a2d2ac272ad140be07e3be334ac6d129
Alfredo Alegría <150471094+alfredoalegria@users.noreply.github.com>
2024-04-25 16:32:13 -0400

    Update empirical_variog.R

--- empirical_variog.R
+++ empirical_variog.R
@@ -8,8 +8,8 @@ source("/basic_functions/functions.R")
 source("/basic_functions/empVariog.R")
 source("/sim_algorithms/auxiliary_process.R")
 source("/sim_algorithms/SpectralSim.R")
-source("/sim_algorithms/Dilution2Sim.R")
 source("/sim_algorithms/Dilution1Sim.R")
+source("/sim_algorithms/Dilution2Sim.R")
 set.seed(23);
 
 ## Chicago network
