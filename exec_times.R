662acf43f0e815472040daf0d14b74b8601ac9ee
Alfredo Alegría <150471094+alfredoalegria@users.noreply.github.com>
2024-04-25 16:23:34 -0400

    Update exec_times.R

--- exec_times.R
+++ exec_times.R
@@ -10,18 +10,19 @@ source("/Users/alfredo/Desktop/codes_final/sim_algorithms/Dilution1Sim.R")
 source("/Users/alfredo/Desktop/codes_final/basic_functions/plotSim.R")
 set.seed(23);
 
+## Chicago network
 data(chicago);
 Net=chicago$domain;
 
+## Number of target points
 k=2^(5:10);
-time=c();
+time=c();   # it will contain the execution times
 for(j in 1:length(k)){
-	nPointsE=rep(k[j],nsegments(Net));
+      nPointsE=rep(k[j],nsegments(Net));
       ini=Sys.time();   
-          SimSpec(Net,nPointsE,nCopies=1000,scaleParam=0.2);
+      SimSpec(Net,nPointsE,nCopies=1000,scaleParam=0.2);
       fin=Sys.time();
-    time[j]=fin-ini;
-    print(time[j]);
+      time[j]=fin-ini;
 }
 
 
