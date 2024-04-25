e3a438848809958d0599ff46e8537a5866349e7f
Alfredo Alegría <150471094+alfredoalegria@users.noreply.github.com>
2024-04-25 16:31:53 -0400

    Update exec_times.R

--- exec_times.R
+++ exec_times.R
@@ -5,8 +5,8 @@ source("/basic_functions/r/BB.R")
 source("/basic_functions/functions.R")
 source("/sim_algorithms/auxiliary_process.R")
 source("/sim_algorithms/SpectralSim.R")
-source("/sim_algorithms/Dilution2Sim.R")
 source("/sim_algorithms/Dilution1Sim.R")
+source("/sim_algorithms/Dilution2Sim.R")
 set.seed(23);
 
 ## Chicago network
