36fe85987616d5409f742a3ddfb2e5614c53b799
Alfredo Alegría <150471094+alfredoalegria@users.noreply.github.com>
2024-04-25 20:33:12 -0400

    Update BB.c

--- basic_functions/c/BB.c
+++ basic_functions/c/BB.c
@@ -21,14 +21,12 @@ void BB(int *nEdges,double *lenEdges,int *nPointsE,double *vecNorm,double *vec)
          {    
 			  if(j==(n-1)){vec[id]=0;}
 			  else{
-			     tj=j*T/(n-1);
-			     ta=(j-1)*T/(n-1);
-			     
-                 aux=(T-2*tj+ta)/(T-ta);
-                 vec[id]=0.5*((1+aux)*vec[id-1]+vecNorm[id]*pow(T-ta-(T-2*tj+ta)*aux,0.5));
-              }
-              
-              id=id+1;
+			         tj=j*T/(n-1);
+			         ta=(j-1)*T/(n-1);
+                                 aux=(T-2*tj+ta)/(T-ta);
+                                 vec[id]=0.5*((1+aux)*vec[id-1]+vecNorm[id]*pow(T-ta-(T-2*tj+ta)*aux,0.5));
+                          }
+                          id=id+1;
          }
      }                  
  
